4a08bad8d6bac5668b3280dbed814040bf68691d
GAETAN-R-BLUE <gaetanrodrigue_98@yahoo.com>
2025-04-26 08:52:05 -0400

    This is for my ObjectOriented programming

--- src/EmployeeMain.java
+++ src/EmployeeMain.java
@@ -7,15 +7,15 @@ public class EmployeeMain { // Main class containing the entry point of the prog
 
     public static void main(String[] args) {
         // Create the first Employee object
-        Employee employee = new Employee(361361, "Rodrigue", "Gaetan",
-                "gaetanrodrigue98@gmail.com", "Engineering");
+        Employee employee1 = new Employee(361361, "Rodrigue", "Gaetan",
+                35,"gaetanrodrigue98@gmail.com", "Engineering", 250458.25);
 
         // Create the second Employee object with the same data
-        Employee employ = new Employee(361361, "Rodrigue", "Gaetan",
-                "gaetanrodrigue98@gmail.com", "Engineering");
+        Employee employee2 = new Employee(361361, "Rodrigue", "Gaetan",
+                26,"gaetanrodrigue98@gmail.com", "Engineering",75268.152);
 
         // Compare the two Employee objects using the equals() method
         // and print the result (true or false)
-        System.out.println(employee.equals(employ));
+        System.out.println(employee1.equals(employee2));
     }
 }
--- src/MyEmployees/Employee.java
+++ src/MyEmployees/Employee.java
@@ -1,23 +1,47 @@
-package MyEmployees;
+package MyEmployees; // Package name
 
+// Employee class to model an employee's information
 public class Employee {
-    // The following are the properties of the employee class
-    private int  employeeID;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String department;
-    // The following is the constructor of my class
-    public Employee (int employeeID, String firstName, String lastName, String email, String department  ){
+    // Properties (fields) of the Employee class
+    private int employeeID; // Unique identifier for employee
+    private String firstName; // Employee's first name
+    private Integer age;
+    private String lastName; // Employee's last name
+    private String email; // Employee's email address
+    private String department; // Department employee belongs to
+    private double salary; // Employee's salary
+
+    // Constructor to initialize Employee object without salary
+    public Employee(int employeeID, String firstName, String lastName,Integer age, String email, String department, Double salary) {
         this.employeeID = employeeID;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.age = age;
         this.email = email;
         this.department = department;
+        this.salary = salary;
+    }
+    // Getter for age
+    public int  getAge() {
+        return age;
     }
 
-    // The following are the setter and the getter for my employee class
+    // Setter for salary
+    public void setAge(int age) {
+        this.age = age;
+    }
 
+    // Getter for salary
+    public double getSalary() {
+        return salary;
+    }
+
+    // Setter for salary
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    // Getter and Setter methods for other fields
     public int getEmployeeID() {
         return employeeID;
     }
@@ -57,13 +81,15 @@ public class Employee {
     public void setDepartment(String department) {
         this.department = department;
     }
+
+    // toString() method to display employee information nicely
     @Override
     public String toString() {
         return "Employee Details:\n" +
                 "ID: " + employeeID + "\n" +
                 "Name: " + firstName + " " + lastName + "\n" +
                 "Email: " + email + "\n" +
-                "Department: " + department;
+                "Department: " + department + "\n" +
+                "Salary: " + salary;
     }
 }
-
