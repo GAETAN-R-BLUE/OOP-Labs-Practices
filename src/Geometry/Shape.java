e383c0537dd795b3c2b5fe8f053b2cfcc1294585
GAETAN-R-BLUE <gaetanrodrigue_98@yahoo.com>
2025-05-05 21:40:35 -0400

    This is for my ObjectOriented programming

---
+++ src/Geometry/Shape.java
@@ -0,0 +1,34 @@
+package Geometry;
+
+public class Shape {
+    private String name, color;
+
+    public Shape(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
